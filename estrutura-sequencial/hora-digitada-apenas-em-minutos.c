4811ad50b4475f80db47ab6530f381019cc1f9e3
Gregorio Ferreira <gregorioagostinho@gmail.com>
2025-03-12 15:21:50 +0100

    calculo do minuto

---
+++ estrutura-sequencial/hora-digitada-apenas-em-minutos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double numero = 12.345;
+    int parte_inteira = (int)numero; // Cast para int
+
+    printf("Parte inteira: %d\n", parte_inteira); // Saída: 12
+    return 0;
+}
